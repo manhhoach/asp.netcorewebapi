953db8e4534b8d629fb5b5eb898309db6ba6884c
manhhoach <manhhn.nv5@gmail.com>
2024-05-18 10:32:52 +0700

    .

--- CompanyEmployees/ContextFactory/RepositoryContextFactory.cs
+++ CompanyEmployees/ContextFactory/RepositoryContextFactory.cs
@@ -1,6 +1,20 @@
-﻿namespace CompanyEmployees.ContextFactory
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Repository;
+
+namespace CompanyEmployees.ContextFactory
 {
-    public class RepositoryContextFactory
+    public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
     {
+        public RepositoryContext CreateDbContext(string[] args)
+        {
+            var config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+            var builder = new DbContextOptionsBuilder<RepositoryContext>().UseSqlServer(
+                config.GetConnectionString("sqlConnection"),
+                b => b.MigrationsAssembly("CompanyEmployees")
+                );
+
+            return new RepositoryContext(builder.Options);
+        }
     }
 }
