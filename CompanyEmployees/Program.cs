072d45abf30d70494a1d8a76a98e46dd8650f538
manhhoach <manhhn.nv5@gmail.com>
2024-06-02 16:12:03 +0700

    add filter

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Options;
 using NLog;
+using Presentation.ActionFilters;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -44,6 +45,8 @@ builder.Services.ConfigureCors();
 builder.Services.ConfigureIISIntegration();
 builder.Services.ConfigureLoggerService();
 builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddScoped<ValidationFilterAttribute>();
+
 
 var app = builder.Build();
 
--- Presentation/Controllers/CompaniesController.cs
+++ Presentation/Controllers/CompaniesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Presentation.ActionFilters;
 using Presentation.ModelBinders;
 using Service.Contracts;
 using Shared.DataTransferObjects;
@@ -30,14 +31,15 @@ namespace Presentation.Controllers
         }
 
         [HttpPost]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
         public async Task<IActionResult> CreateCompany([FromBody] CompanyForCreationDto company)
         {
-            if (company is null)
-            {
-                return BadRequest("CompanyForCreationDto object is null");
-            }
-            if (!ModelState.IsValid)
-                return UnprocessableEntity(ModelState);
+            //if (company is null)
+            //{
+            //    return BadRequest("CompanyForCreationDto object is null");
+            //}
+            //if (!ModelState.IsValid)
+            //    return UnprocessableEntity(ModelState);
             var createdCompany = await _serviceManager.Company.CreateCompanyAsync(company);
             return CreatedAtRoute("CompanyById", new { id = createdCompany.Id }, createdCompany);
         }
@@ -65,14 +67,14 @@ namespace Presentation.Controllers
         }
 
         [HttpPut("{id:guid}")]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
         public async Task<IActionResult> UpdateCompany(Guid id, [FromBody] CompanyForUpdateDto company)
         {
-            if (company is null)
-                return BadRequest("CompanyForUpdateDto object is null");
-            if (!ModelState.IsValid)
-                return UnprocessableEntity(ModelState);
+            //if (company is null)
+            //    return BadRequest("CompanyForUpdateDto object is null");
+            //if (!ModelState.IsValid)
+            //    return UnprocessableEntity(ModelState);
             await _serviceManager.Company.UpdateCompanyAsync(id, company, trackChanges: true);
-
             return NoContent();
         }
     }
